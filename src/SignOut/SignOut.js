34aaec54657116c21dffca02be7d4d7e3112471b
Sandeep Ashok <51964832+sandeepashok@users.noreply.github.com>
2021-06-16 23:56:40 +0530

    fix: signout button

--- src/SignOut/SignOut.js
+++ src/SignOut/SignOut.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { auth } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import Button from '@material-ui/core/Button';
 
 export default function SignOut() {
-  return auth.currentUser && (
+  const [user] = useAuthState(auth)
+  return user && (
 
     <Button color="primary" onClick={() => auth.signOut()}>Sign Out</Button>
 
